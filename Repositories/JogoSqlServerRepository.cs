d3db2b07ed8af963923e430875550ee22402b11e
Mateus Oliveira <mthenriqueo@gmail.com>
2021-11-20 00:02:42 -0300

    Projeto API Catalogo de Jogos

---
+++ Repositories/JogoSqlServerRepository.cs
@@ -0,0 +1,134 @@
+﻿using ApiCatalogoJogos.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public class JogoSqlServerRepository : IJogoRepository
+    {
+        private readonly SqlConnection sqlConnection;
+
+        public JogoSqlServerRepository(IConfiguration configuration)
+        {
+            sqlConnection = new SqlConnection(configuration.GetConnectionString("Default"));
+        }
+
+        public async Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            var jogos = new List<Jogo>();
+
+            var comando = $"SELECT * FROM Jogos ORDER BY id OFFSET {((pagina - 1) * quantidade)} rows fetch next {quantidade} rows only";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read())
+            {
+                jogos.Add(new Jogo
+                {
+                    Id = (Guid)sqlDataReader["Id"],
+                    Nome = (string)sqlDataReader["Nome"],
+                    Produtora = (string)sqlDataReader["Produtora"],
+                    Preco = (double)sqlDataReader["Preco"]
+                });
+            }
+
+            await sqlConnection.CloseAsync();
+
+            return jogos;
+        }
+
+        public async Task<Jogo> Obter(Guid id)
+        {
+            Jogo jogo = null;
+
+            var comando = $"SELECT * FROM Jogos WHERE Id = '{id}'";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read())
+            {
+                jogo = new Jogo
+                {
+                    Id = (Guid)sqlDataReader["Id"],
+                    Nome = (string)sqlDataReader["Nome"],
+                    Produtora = (string)sqlDataReader["Produtora"],
+                    Preco = (double)sqlDataReader["Preco"]
+                };
+            }
+
+            await sqlConnection.CloseAsync();
+
+            return jogo;
+        }
+
+        public async Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            var jogos = new List<Jogo>();
+
+            var comando = $"SELECT * FROM Jogos WHERE Nome =  '{nome}' AND Produtora = '{produtora}'";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+            while (sqlDataReader.Read())
+            {
+                jogos.Add(new Jogo
+                {
+                    Id = (Guid)sqlDataReader["Id"],
+                    Nome = (string)sqlDataReader["Nome"],
+                    Produtora = (string)sqlDataReader["Produtora"],
+                    Preco = (double)sqlDataReader["Preco"]
+                });
+            }
+
+            await sqlConnection.CloseAsync();
+
+            return jogos;
+        }
+
+        public async Task Inserir(Jogo jogo)
+        {
+            var comando = $"INSERT Jogos (Id, Nome, Produtora, Preco) VALUES ('{jogo.Id}', '{jogo.Nome}', '{jogo.Produtora}', {jogo.Preco.ToString().Replace(",", ".")})";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            await sqlConnection.CloseAsync();
+        }
+
+        public async Task Atualizar(Jogo jogo)
+        {
+            var comando = $"UPDATE Jogos SET Nome = '{jogo.Nome}', '{jogo.Produtora}', {jogo.Preco.ToString().Replace(",", ".")}";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            await sqlConnection.CloseAsync();
+        }
+
+        public async Task Remover(Guid id)
+        {
+            var comando = $"DELETE FROM Jogos WHERE ID = '{id}'";
+
+            await sqlConnection.OpenAsync();
+            SqlCommand sqlCommand = new SqlCommand(comando, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            await sqlConnection.CloseAsync();
+        }
+
+        public void Dispose()
+        {
+            sqlConnection?.Close();
+            sqlConnection?.Dispose();
+        }
+    }
+}
